5cce3db2affc330499655ea5df2cfe5192b63020
Anweshan Roy Chowdhury <anweshanrc15@gmail.com>
2022-05-07 19:41:18 +0530

    Modified: app.py
    Added blur & blur skew (amount of blur) as an image option, with few other changes.

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-import zipfile, tempfile, fitz
-from flask import Flask, request, make_response, render_template
+import cv2, fitz, numpy as np, os, tempfile, zipfile
+from dotenv import load_dotenv
+from flask import Flask, make_response, request
 
+load_dotenv()
+
+CLIENT_ID, CLIENT_SECRET = os.getenv("CLIENT_ID"), os.getenv("CLIENT_SECRET")
 app = Flask(__name__)
+
+
+def blur_image(img, ksize=(10, 10), output='png'):
+    print(ksize)
+    copy_img = img
+    try:
+        if isinstance(img, bytes):
+            np_arr = np.frombuffer(img, np.uint8)
+            img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)  # cv2.IMREAD_COLOR in OpenCV 3.1
+        assert isinstance(img, np.ndarray)
+        img = cv2.blur(img, ksize, cv2.BORDER_DEFAULT)
+        assert isinstance(img, np.ndarray)
+        img = cv2.imencode("." + output, img)[1].tobytes()
+        return img
+    except AssertionError:
+        return copy_img
 
 
 @app.route('/upload-url', methods=['GET', 'POST'])
@@ -9,34 +29,18 @@ def upload_url():
     page_number_arg, bundle_arg, url_arg = [0], False, None
 
     if 'url' in request.args and len(request.args.get('url')) > 0:
-        # TODO: DOWNLOAD THE CONTENT THE URL.
+        # TODO: DOWNLOAD THE CONTENT FROM THE URL.
         pass
     else:
         return 'URL is expected', 400
 
-    if 'page' in request.args:
-        page_number_arg = request.args.get('page')
-        if not page_number_arg.isnumeric():
-            return 'Page Number should be numeric', 400
-        page_number_arg = [0, int(page_number_arg)]
 
-    if 'bundle' in request.args:
-        bundle_arg = request.args.get('bundle')
-        if bundle_arg.isnumeric() and (int(bundle_arg) == 1 or int(bundle_arg) == 0):
-            if int(bundle_arg) == 1:
-                # Right side: page number given to Last Page
-                page_number_arg = [max(page_number_arg)]
-            bundle_arg = True
-        else:
-            return 'Bundle Number should be 0 or 1', 400
-
-    msg = 'Some Internal Error Occurred', 500
 
 
 # noinspection PyBroadException
 @app.route('/upload', methods=['POST'])
 def upload():
-    page_number_arg, bundle_arg = [0], False
+    page_number_arg, bundle_arg, blur_arg, blur_skew = [0], False, False, (10, 10)
 
     if not (request.method == 'POST' and 'file' in request.files):
         return 'The protocol is invalid.', 405
@@ -67,6 +71,21 @@ def upload():
         else:
             return 'Bundle Number should be 0 or 1', 400
 
+    if 'blur' in request.args:
+        blur_arg = request.args.get('blur')
+        blur_skew = request.args.get('blur_skew') or (10, 10)
+
+        if not isinstance(blur_skew, tuple):
+            blur_skew = (int(blur_skew), int(blur_skew))
+
+        if blur_arg.isnumeric() and (int(blur_arg) == 1 or int(blur_arg) == 0):
+            if int(blur_arg) == 1:
+                blur_arg = True
+            else:
+                blur_arg = False
+        else:
+            return 'Blur should be 0 or 1', 400
+
     msg = 'Some Internal Error Occurred', 500
     try:
         print("Document is being read!! Upload: Success")
@@ -92,6 +111,9 @@ def upload():
         if not bundle_arg:
             pdf_page = document.load_page(page_number)
             pdf_page_pix = (pdf_page.get_pixmap()).tobytes(output='png')
+
+            if blur_arg:
+                pdf_page_pix = blur_image(pdf_page_pix, output='png', ksize=blur_skew)
 
             img_response = make_response(pdf_page_pix)
             img_response.headers['Content-Type'] = 'image/png'
@@ -129,4 +151,4 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, debug=True)
